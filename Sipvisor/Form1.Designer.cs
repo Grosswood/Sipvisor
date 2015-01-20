a6608448ce25ee3c88a4e67075ade4a204961542
Michael Siplatov <mozge4ok@gmail.com>
2015-01-20 15:45:36 +0500

    Basic design and first four questions

--- Sipvisor/Form1.Designer.cs
+++ Sipvisor/Form1.Designer.cs
@@ -28,12 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "У Вас слабые конкуренты";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Вы можете модернизировать производство";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Вас поддерживает правительство";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Можете вести маркетинговые исслед.";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.ColumnWidth = 45;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBox1.Location = new System.Drawing.Point(336, 129);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(236, 21);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Что делать?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Совет";
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.ColumnWidth = 45;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBox2.Location = new System.Drawing.Point(336, 156);
+            this.checkedListBox2.MultiColumn = true;
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(236, 21);
+            this.checkedListBox2.TabIndex = 10;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.CheckOnClick = true;
+            this.checkedListBox3.ColumnWidth = 45;
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBox3.Location = new System.Drawing.Point(336, 183);
+            this.checkedListBox3.MultiColumn = true;
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(236, 21);
+            this.checkedListBox3.TabIndex = 11;
+            this.checkedListBox3.SelectedIndexChanged += new System.EventHandler(this.checkedListBox3_SelectedIndexChanged);
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.CheckOnClick = true;
+            this.checkedListBox4.ColumnWidth = 45;
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBox4.Location = new System.Drawing.Point(336, 210);
+            this.checkedListBox4.MultiColumn = true;
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(236, 21);
+            this.checkedListBox4.TabIndex = 12;
+            this.checkedListBox4.SelectedIndexChanged += new System.EventHandler(this.checkedListBox4_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(347, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Да";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(527, 109);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Нет";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(58, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(541, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Вас приветствует программа базового SWOT-анализа Sipvisor!";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(614, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Пожалуйста, оцените свою организацию по нижеследующим критериям по шкале от 1 до " +
+    "5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(662, 387);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sipvisor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+
     }
 }
 
--- Sipvisor/Form1.cs
+++ Sipvisor/Form1.cs
@@ -15,6 +15,115 @@ namespace Sipvisor
         public Form1()
         {
             InitializeComponent();
+            this.label5.Text = "";
+        }
+
+        //Variables for answers
+
+        private int answerOne;
+        private int answerTwo;
+        private int answerThree;
+        private int answerFour;
+
+        //Converting position of checked boxes into int variables created above
+
+        private void gatherAnswers()
+        {
+            foreach (int indexChecked in checkedListBox1.CheckedIndices)
+            {
+                answerOne = indexChecked;
+            }
+            foreach (int indexChecked in checkedListBox2.CheckedIndices)
+            {
+                answerTwo = indexChecked;
+            }
+            foreach (int indexChecked in checkedListBox3.CheckedIndices)
+            {
+                answerThree = indexChecked;
+            }
+            foreach (int indexChecked in checkedListBox4.CheckedIndices)
+            {
+                answerFour = indexChecked;
+            }
+        }
+
+        //Chooses advices by answers
+
+        private void displayAdvice()
+        {
+
+            string[] minAdvice = { "Увеличивайте ассортимент продукции", "Расширяйте ассортимент продукции", "Старайтесь соблюдать законы", "Следите за продукцией конкурентов" };
+            string[] maxAdvice = { "Занимайте свободные сегменты рынка", "Модернизируйте производство", "Возьмите кредит на развитие", "Следуйте вкусам потребителей" };
+
+            int[] answers = { answerOne, answerTwo, answerThree, answerFour };
+            
+            int[] minAdviceToChoose = { 0, 5 }; //number, value
+            int[] maxAdviceToChoose = { 0, 0 }; //number, value
+
+            for (int answerNumber = 0; answerNumber < 4; answerNumber++)
+            {
+                if (minAdviceToChoose[1] > answers[answerNumber])
+                {
+                    minAdviceToChoose[0] = answerNumber;
+                    minAdviceToChoose[1] = answers[answerNumber];
+                }
+                if (maxAdviceToChoose[1] < answers[answerNumber])
+                {
+                    maxAdviceToChoose[0] = answerNumber;
+                    maxAdviceToChoose[1] = answers[answerNumber];
+                }
+            }
+
+            this.label5.Text = minAdvice[minAdviceToChoose[0]] + " и " + maxAdvice[maxAdviceToChoose[0]];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.label9.Visible = false;
+            if (checkedListBox1.SelectedItems.Count == 1 && checkedListBox2.SelectedItems.Count == 1 && checkedListBox3.SelectedItems.Count == 1 && checkedListBox4.SelectedItems.Count == 1)
+            {
+                gatherAnswers();
+                displayAdvice();
+            }
+            else
+            {
+                this.label5.Text = "Вам нужно поставить галочку напротив каждого ответа";
+            }
+            //this.label5.Text = answerOne.ToString() + answerTwo.ToString() + answerThree.ToString() + answerFour.ToString();
+        }
+
+
+        //Uncheck other boxes on check
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int itemToUncheck = 0; itemToUncheck < checkedListBox1.Items.Count; itemToUncheck++)
+            {
+                checkedListBox1.SetItemChecked(itemToUncheck, false);
+            }
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int itemToUncheck = 0; itemToUncheck < checkedListBox2.Items.Count; itemToUncheck++)
+            {
+                checkedListBox2.SetItemChecked(itemToUncheck, false);
+            }
+        }
+
+        private void checkedListBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int itemToUncheck = 0; itemToUncheck < checkedListBox3.Items.Count; itemToUncheck++)
+            {
+                checkedListBox3.SetItemChecked(itemToUncheck, false);
+            }
+        }
+
+        private void checkedListBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            for (int itemToUncheck = 0; itemToUncheck < checkedListBox4.Items.Count; itemToUncheck++)
+            {
+                checkedListBox4.SetItemChecked(itemToUncheck, false);
+            }
         }
     }
 }
